7a9af4a13ee829a2c77d19de0f10a6772223f61e
Alexandrade01 <solidwinter96@outlook.com>
2024-01-29 22:49:53 -0300

    criando objetos no javascript

---
+++ 02-03 - objetos/script.js
@@ -0,0 +1,14 @@
+var person = {
+
+  name:'Alexandre',
+  age: 27,
+  sex: 'male'
+}
+
+console.log(person)
+
+//propriedade do objeto
+console.log(person.name)
+
+person.age = 'BRASIL'
+console.log(person.age)
