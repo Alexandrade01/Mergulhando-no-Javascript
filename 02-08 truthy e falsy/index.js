4fb92bc0996a7ffa25839dfe3a3b348e4ea49042
Alexandrade01 <solidwinter96@outlook.com>
2024-02-01 23:12:58 -0300

    entendendo valores de true no js

---
+++ 02-08 truthy e falsy/index.js
@@ -0,0 +1,16 @@
+//existem varios valores considerados truthy, pesquisar a tabela
+
+var condition = undefined
+
+var value = true
+
+console.log(!value)
+
+if(condition){
+
+  console.log('A condição é verdadeira')
+}
+
+else{
+  console.log('A condição é falsa')
+}
