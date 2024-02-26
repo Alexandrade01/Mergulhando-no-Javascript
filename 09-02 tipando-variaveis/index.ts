24979af0f388ee7dfab8027e63aa9aa02d35dfe0
Alexandrade01 <solidwinter96@outlook.com>
2024-02-26 00:16:05 -0300

    introducao-ao-typescript

---
+++ 09-02 tipando-variaveis/index.ts
@@ -0,0 +1,7 @@
+//apos os dois pontos é possivel definir o tipo, o typescript tem como diferencial a tipagem 
+const personName:String = 'Alexandre'
+const personAge:Number = 25
+const personAlive:boolean = false
+const personContacts:string[] = ['a',"b",`c`]
+
+const personContactsStringandNumbers:(string|number)[] = ['a',"b",0]  
