ecc4a8bdd881ca81c9ce4a14fec0d58f56987de5
Alexandrade01 <solidwinter96@outlook.com>
2024-02-28 15:44:35 -0300

    type-aliasing-typescript

---
+++ 09-03 type-aliasing/index.js
@@ -0,0 +1,7 @@
+"use strict";
+//apos os dois pontos é possivel definir o tipo, o typescript tem como diferencial a tipagem 
+const personName = 'Alexandre';
+const personAge = 25;
+const personAlive = false;
+const personContacts = ['a', "b", `c`];
+const personContactsStringandNumbers = ['a', "b", 0];
---
+++ 09-03 type-aliasing/index.ts
@@ -0,0 +1,10 @@
+//criação de types e junção de diferentes types
+type CPF = string
+type Dados = string|number
+
+
+const cpf:CPF = 'texto'
+
+type Sex = 'male' | 'female'
+const sex:Sex = "female"
+
